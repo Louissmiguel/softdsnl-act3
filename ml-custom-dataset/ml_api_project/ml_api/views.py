eba0de1723d3779057340422ee2722d5f3f16654
esir-ops <re.sicat.3104@gmail.com>
2025-07-16 09:52:25 +0800

    act3

--- ml-custom-dataset/ml_api_project/ml_api/views.py
+++ ml-custom-dataset/ml_api_project/ml_api/views.py
@@ -1,25 +1,27 @@
-from django.shortcuts import render
-
-# Create your views here.
-# ml_api/views.py
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import joblib
+from django.conf import settings
+import os
 
-model = joblib.load("model.pkl")
-label_encoder = joblib.load("label_encoder.pkl")
+# Load model and encoder using correct paths
+model_path = os.path.join(settings.BASE_DIR, "ml_api", "model.pkl")
+encoder_path = os.path.join(settings.BASE_DIR, "ml_api", "label_encoder.pkl")
+
+# Load model and label encoder
+model = joblib.load(model_path)
+label_encoder = joblib.load(encoder_path)
 
 class PredictView(APIView):
     def post(self, request):
         try:
-            petal_length = float(request.data.get("petal_length"))
-            petal_width = float(request.data.get("petal_width"))
+            tail_length = float(request.data.get("tail_length"))
+            ear_size = float(request.data.get("ear_size"))
 
-            prediction = model.predict([[petal_length, petal_width]])
-            label = label_encoder.inverse_transform(prediction)[0]
+            prediction = model.predict([[tail_length, ear_size]])
+            breed_label = label_encoder.inverse_transform(prediction)[0]
 
-            return Response({"prediction": label})
+            return Response({"prediction": breed_label})
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
